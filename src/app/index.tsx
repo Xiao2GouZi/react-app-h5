0d0554ea9afb17929a6b936f7536b0201dfc7534
齐云猛 <bank@qiyunmengdeMacBook-Pro.local>
2019-07-24 22:50:44 +0800

    base

---
+++ src/app/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+
+
+export default class Index extends React.PureComponent {
+
+    render(){
+        return(
+            <div>爱上了大几十块</div>
+        )
+    }
+
+}
